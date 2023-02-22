81a36c24091fa84d8b3fa60ec21295600668c07e
Tommo <tommos.tools@gmail.com>
2023-02-22 08:58:51 +0000

    the thick plottens

--- packages/contextors/src/test3b.ts
+++ packages/contextors/src/test3b.ts
@@ -1,45 +1,33 @@
 import react from "react";
 
-type Tuple<T> = [] | [T, ...T[]];
-
 type Wrapped<T> = { $$value: T; $$wrapped: true; }
 
 function wrap<T>(value: T): Wrapped<T> { return { $$value: value, $$wrapped: true }; }
 function unwrap<T>(wrapped: Wrapped<T>) { return wrapped.$$value; }
 function isWrapped(value: any): value is Wrapped<any> { return "$$wrapped" in value; }
 
-type Combiner<Output, Arg>          = (arg: Arg) => Output;
-type WrappedOrCombiner<Output, Arg> = Wrapped<Output> | Combiner<Output, Arg>
+type Evaluator<Output, Arg> = (arg: Arg) => Output;
 
-type OutputFor<T>   = T extends WrappedOrCombiner<infer Output, {}> ? Output : never;
-type OutputsFor<TT> = { [K in keyof TT]: OutputFor<TT[K]> };
+type EvaluatorOrWrappedValue<Output, Arg> = Wrapped<Output> | Evaluator<Output, Arg>
 
-type UnionToIntersection<T> = (T extends any ? (x: T) => any : never) extends (x: infer R) => any ? R : never;
+type OutputFor<Input> = Input extends EvaluatorOrWrappedValue<infer Output, {}> ? Output : never;
+type ArgFor<Input>    = Input extends Evaluator<{}, infer Arg> ? Arg : {}; // if this is `any` then it passes but Arg is discarded
 
-
-type ArgsFor<Inputs extends Tuple<WrappedOrCombiner<{}, {}>>> = Inputs extends infer InputsT ? {
-	[Index in keyof InputsT]: (
-		InputsT[Index] extends WrappedOrCombiner<any, infer Arg> ? Arg : InputsT[Index]
-	)
-} : never;
-
-type ArgFor<Inputs extends Tuple<WrappedOrCombiner<{}, {}>>> =
-	UnionToIntersection<ArgsFor<Inputs>[number]>;
-
-type SingleArgFor<Input> =
-    Input extends Combiner<{}, infer Arg> ? Arg : {};
-
-function makeCombiner<Output, Inputs extends WrappedOrCombiner<{}, Arg>, Arg extends {}>(
-	combine: (arg: Arg & SingleArgFor<Inputs>, values: OutputFor<Inputs>) => Output,
-    inputs: Inputs
-): Combiner<Output, Arg & SingleArgFor<Inputs>>
+function makeEvaluator<
+    Output,
+    Input extends EvaluatorOrWrappedValue<{}, Arg>,
+    Arg extends {}
+>(
+	evaluate: (arg: Arg & ArgFor<Input>, values: OutputFor<Input>) => Output,
+    input: Input
+): Evaluator<Output, Arg & ArgFor<Input>>
 {
-	return (arg: Arg & SingleArgFor<Inputs>) => {
-		const values = (isWrapped(inputs) ? unwrap(inputs) : inputs(arg));
-		return combine(arg, values as OutputFor<Inputs>);
+	return (arg) => {
+		const value = (isWrapped(input) ? unwrap(input) : input(arg));
+		return evaluate(arg, value as OutputFor<Input>);
 	}
 }
 
-const Context1 = wrap({ a: 5 });
-const CX1 = makeCombiner((arg: { c: number }, s) => 3, Context1);
-const ctxinput = makeCombiner((arg, v1) => null, CX1);
+const wrappedValue1 = wrap({ a: 5 });
+const evaluator1 = makeEvaluator((arg: { c: number }, s) => 3, wrappedValue1);
+const ctxinput = makeEvaluator((arg, v1) => null, evaluator1);
