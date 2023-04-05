7be03047e95cd0e0b26d106f1538a705f10e886f
Tommo <tommos.tools@gmail.com>
2023-04-05 22:04:13 +0100

    lint some more tests ...

--- packages/contexto/src/__tests__/api.tsx
+++ packages/contexto/src/__tests__/api.tsx
@@ -1,11 +1,23 @@
+/* eslint-disable react/function-component-definition */
+
 /**
  * @jest-environment jsdom
  */
 
 import { render } from "@testing-library/react";
-import { ReactNode, useEffect, useState, createContext as createReactContext, useRef } from "react";
+import {
+	ReactNode,
+	createContext as createReactContext,
+	useEffect,
+	useState,
+} from "react";
 import { act } from "react-dom/test-utils";
-import { createContext, createProxyContext, useContextUpdate, useSubscriber } from "..";
+import {
+	createContext,
+	createProxyContext,
+	useContextUpdate,
+	useSubscriber,
+} from "..";
 
 const Context1 = createContext(111, { contextId: "context1" });
 const Context2 = createContext("two", { contextId: "context2" });
@@ -14,143 +26,152 @@ const RawContext = createReactContext(-1);
 const Context3 = createProxyContext(RawContext, { contextId: "context3" });
 
 test("useSubscriber", async () =>
+{
+	let value1 = 0;
+	let updateCount1 = 0;
+	const listener1 = (newValue: number) =>
 	{
-		let value1 = 0;
-		let updateCount1 = 0;
-		const listener1 = (newValue: number) =>
+		value1 = newValue;
+		updateCount1 += 1;
+	};
+
+	let value2 = "";
+	let updateCount2 = 0;
+	const listener2 = (newValue: string) =>
+	{
+		value2 = newValue;
+		updateCount2 += 1;
+	};
+
+	let updateCount3 = 0;
+	const listener3 = () =>
+	{
+		updateCount3 += 1;
+	};
+
+	let propUpdate: (val: number) => void;
+	let hookUpdate: (val: number) => void;
+
+	const Provider1 = ({ children }: { children: ReactNode }) =>
+	{
+		const [val, setVal] = useState(222);
+		propUpdate = setVal;
+		return <Context1.Provider children={children} value={val} />;
+	};
+
+	const Updater1 = () =>
+	{
+		hookUpdate = useContextUpdate(Context1);
+		return null;
+	};
+
+	const Subscriber = () =>
+	{
+		const subscribe = useSubscriber();
+
+		useEffect(
+			() =>
 			{
-				value1 = newValue;
-				++updateCount1;
-			};
-		let unsubscribe1: () => void;
+				[value1] = subscribe(Context1, listener1);
+				[value2] = subscribe(Context2, listener2);
+				subscribe(Context1, listener3);
+			},
+			[subscribe]
+		);
 
-		let value2 = "";
-		let updateCount2 = 0;
-		const listener2 = (newValue: string) =>
-			{
-				value2 = newValue;
-				++updateCount2;
-			};
-		let unsubscribe2: () => void;
+		return null;
+	};
 
-		let updateCount3 = 0;
-		const listener3 = () => { ++updateCount3; }
-		
-		let propUpdate: (val: number) => void = () => {};
-		let hookUpdate: (val: number) => void = () => {};
+	render(
+		<Provider1>
+			<Updater1 />
+			<Subscriber />
+		</Provider1>
+	);
 
-		const Provider1 = ({ children }: { children: ReactNode }) =>
-			{
-				const [val, setVal] = useState(222);
-				propUpdate = setVal;
-				return <Context1.Provider children={children} value={val} />
-			}
-		
-		const Updater1 = () =>
-			{
-				hookUpdate = useContextUpdate(Context1);
-				return null;
-			}
+	expect(value1).toBe(222);
+	expect(value2).toBe("two");
+	expect(updateCount1).toBe(0);
+	expect(updateCount2).toBe(0);
 
-		const Subscriber = () =>
-			{
-				const subscribe = useSubscriber();
+	act(() => propUpdate(333));
+	expect(value1).toBe(333);
+	expect(updateCount1).toBe(1);
 
-				useEffect(() =>
-					{
-						[value1, unsubscribe1] = subscribe(Context1, listener1);
-						[value2, unsubscribe2] = subscribe(Context2, listener2);
-						subscribe(Context1, listener3);
-					},
-					[]
-				);
+	// setting a prop to the same value should not call listener
+	act(() => propUpdate(333));
+	expect(value1).toBe(333);
+	expect(updateCount1).toBe(1);
 
-				return null;
-			}
-				
-		const Component = () =>
-			<Provider1>
-				<Updater1/>
-				<Subscriber/>
-			</Provider1>
+	act(() => hookUpdate(444));
+	expect(value1).toBe(444);
+	expect(updateCount1).toBe(2);
 
-		render(<Component/>);
+	// updating with hook with same value DOES call the listener again
+	act(() => hookUpdate(444));
+	expect(value1).toBe(444);
+	expect(updateCount1).toBe(3);
 
-		expect(value1).toBe(222);
-		expect(value2).toBe("two");
-		expect(updateCount1).toBe(0);
-		expect(updateCount2).toBe(0);
+	expect(updateCount2).toBe(0);
 
-		act(() => propUpdate(333));
-		expect(value1).toBe(333);
-		expect(updateCount1).toBe(1);
-
-		// setting a prop to the same value should not call listener
-		act(() => propUpdate(333));
-		expect(value1).toBe(333);
-		expect(updateCount1).toBe(1);
-
-		act(() => hookUpdate(444));
-		expect(value1).toBe(444);
-		expect(updateCount1).toBe(2);
-
-		// updating with hook with same value DOES call the listener again
-		act(() => hookUpdate(444));
-		expect(value1).toBe(444);
-		expect(updateCount1).toBe(3);
-
-		expect(updateCount2).toBe(0);
-
-		// multiple listeners to the same context work okay
-		expect(updateCount3).toBe(3);
-	});
+	// multiple listeners to the same context work okay
+	expect(updateCount3).toBe(3);
+});
 
 jest.useFakeTimers();
 
 test("useSubscriber with ProxyContext", async () =>
+{
+	const Provider = ({ children }: { children: ReactNode }) =>
 	{
-		const Provider = ({ children }: { children: ReactNode }) =>
-			{
-				const [tick, setTick] = useState(0);
-				useEffect(() => {
-					const intervalId = setInterval(() => setTick(tick => tick + 1), 1000);
-					return () => clearInterval(intervalId);
-				});
-				return <RawContext.Provider value={tick} children={children} />
-			};
-
-		let updateCount = 0;
-		let sum = 0;
-		const listener = (newValue: number) => {
-			++updateCount;
-			sum += newValue;
-		}
-
-		const Subscriber = () =>
-			{
-				const subscribe = useSubscriber();
-
-				useEffect(() =>
-					{
-						let unsubscribe;
-						[sum, unsubscribe] = subscribe(Context3, listener);
-						return unsubscribe;
-					},
-					[]
-				);
-
-				return null;
-			}
-		
-		render(<Provider><Subscriber/></Provider>);
-
-		expect(updateCount).toBe(0);
-		expect(sum).toBe(0);
-
-		for (let i=1, j=1; i <= 5; ++i, j += i)
+		const [tick, setTick] = useState(0);
+		useEffect(() =>
 		{
-			act(() => jest.advanceTimersByTime(1000));
-			expect(updateCount).toBe(i);
-			expect(sum).toBe(j);
-		}
-	});
+			const intervalId = setInterval(() => setTick((t) => t + 1), 1000);
+			return () => clearInterval(intervalId);
+		});
+		return <RawContext.Provider value={tick} children={children} />;
+	};
+
+	let updateCount = 0;
+	let sum = 0;
+	const listener = (newValue: number) =>
+	{
+		updateCount += 1;
+		sum += newValue;
+	};
+
+	const Subscriber = () =>
+	{
+		const subscribe = useSubscriber();
+
+		useEffect(
+			() =>
+			{
+				let unsubscribe;
+				[sum, unsubscribe] = subscribe(Context3, listener);
+				return unsubscribe;
+			},
+			[subscribe]
+		);
+
+		return null;
+	};
+
+	render(
+		<Provider>
+			<Subscriber />
+		</Provider>
+	);
+
+	expect(updateCount).toBe(0);
+	expect(sum).toBe(0);
+
+	for (let i = 1, j = 1; i <= 5; i += 1, j += i)
+	{
+		// eslint-disable-next-line @typescript-eslint/no-loop-func
+		act(() => jest.advanceTimersByTime(1000));
+		expect(updateCount).toBe(i);
+		expect(sum).toBe(j);
+	}
+});
--- packages/contexto/src/__tests__/basic.tsx
+++ packages/contexto/src/__tests__/basic.tsx
@@ -1,32 +1,46 @@
+/* eslint-disable react/function-component-definition */
+
 /**
  * @jest-environment jsdom
  */
 
-import React, { ReactNode, useEffect, useState } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import { createContext, createProxyContext, useContext, useContexts, useContextUpdate, INHERIT } from "..";
-import { Context } from "../types/";
+import {
+	createContext,
+	createProxyContext,
+	useContext,
+	useContextUpdate,
+	useContexts,
+} from "..";
+import {
+	INHERIT,
+	Context,
+} from "../types";
 
-const Render = <T,>({ context, testId }: { context: Context<T>, testId?: string }) =>
+// eslint-disable-next-line @typescript-eslint/comma-dangle, react/require-default-props
+const Render = <T,>({ context, testId }: { context: Context<T>, testId?: string }) => (
 	<div>
-		<span role="heading">{ context.displayName }</span>
+		<span role="heading" aria-level={1}>{ context.displayName }</span>
 		<span role="definition" data-testid={testId}>{ JSON.stringify(useContext(context)) }</span>
 	</div>
+);
 
 test("Basic SubscriptionContext", () =>
-	{
-		const Context1 = createContext("default value", { displayName: "Context Name", contextId: "test1" });
+{
+	const Context1 = createContext("default value", { displayName: "Context Name", contextId: "test1" });
 
-		render(<Render context={Context1} />);
-		expect(screen.getByRole("heading")).toHaveTextContent("Context Name");
-		expect(screen.getByRole("definition")).toHaveTextContent("default value");
-	});
+	render(<Render context={Context1} />);
+	expect(screen.getByRole("heading")).toHaveTextContent("Context Name");
+	expect(screen.getByRole("definition")).toHaveTextContent("default value");
+});
 
 test("SubscriptionContext with Providers", () =>
-	{
-		const Context1 = createContext("default value", { contextId: "test2" });
-		render(<>
+{
+	const Context1 = createContext("default value", { contextId: "test2" });
+	render(
+		<>
 			<Render context={Context1} testId="Context1.defaultValue" />
 			<Context1.Provider value="value1">
 				<Render context={Context1} testId="Context1.Provider[0]" />
@@ -34,146 +48,157 @@ test("SubscriptionContext with Providers", () =>
 					<Render context={Context1} testId="Context1.Provider[1]" />
 				</Context1.Provider>
 			</Context1.Provider>
-		</>);
-		expect(screen.getByTestId("Context1.defaultValue")).toHaveTextContent("default value");
-		expect(screen.getByTestId("Context1.Provider[0]")).toHaveTextContent("value1");
-		expect(screen.getByTestId("Context1.Provider[1]")).toHaveTextContent("value2");
-	});
+		</>
+	);
+	expect(screen.getByTestId("Context1.defaultValue")).toHaveTextContent("default value");
+	expect(screen.getByTestId("Context1.Provider[0]")).toHaveTextContent("value1");
+	expect(screen.getByTestId("Context1.Provider[1]")).toHaveTextContent("value2");
+});
 
 test("Multiple SubscriptionContexts with Providers", () =>
-	{
-		const Context1 = createContext("default value 1", { contextId: "test3a" });
-		const Context2 = createContext("default value 2", { contextId: "test3b" });
+{
+	const Context1 = createContext("default value 1", { contextId: "test3a" });
+	const Context2 = createContext("default value 2", { contextId: "test3b" });
 
-		render(
-			<Context1.Provider value="value1a">
-				<Render context={Context2} testId="Context2.defaultValue" />
-				<Context2.Provider value="value2a">
-					<Render context={Context1} testId="Context1.Provider[0]" />
-					<Render context={Context2} testId="Context2.Provider[0]" />
-					<Context1.Provider value="value1b">
-						<Render context={Context1} testId="Context1.Provider[1]" />
-						<Render context={Context2} testId="Context2.Provider[1]" />
-						<Context2.Provider value="value2b">
-							<Render context={Context1} testId="Context1.Provider[2]" />
-							<Render context={Context2} testId="Context2.Provider[2]" />
-						</Context2.Provider>
-					</Context1.Provider>
-				</Context2.Provider>
-			</Context1.Provider>
-		);
-		expect(screen.getByTestId("Context2.defaultValue")).toHaveTextContent("default value");
-		expect(screen.getByTestId("Context1.Provider[0]")).toHaveTextContent("value1a");
-		expect(screen.getByTestId("Context2.Provider[0]")).toHaveTextContent("value2a");
-		expect(screen.getByTestId("Context1.Provider[1]")).toHaveTextContent("value1b");
-		expect(screen.getByTestId("Context2.Provider[1]")).toHaveTextContent("value2a");
-		expect(screen.getByTestId("Context1.Provider[2]")).toHaveTextContent("value1b");
-		expect(screen.getByTestId("Context2.Provider[2]")).toHaveTextContent("value2b");
-	});
+	render(
+		<Context1.Provider value="value1a">
+			<Render context={Context2} testId="Context2.defaultValue" />
+			<Context2.Provider value="value2a">
+				<Render context={Context1} testId="Context1.Provider[0]" />
+				<Render context={Context2} testId="Context2.Provider[0]" />
+				<Context1.Provider value="value1b">
+					<Render context={Context1} testId="Context1.Provider[1]" />
+					<Render context={Context2} testId="Context2.Provider[1]" />
+					<Context2.Provider value="value2b">
+						<Render context={Context1} testId="Context1.Provider[2]" />
+						<Render context={Context2} testId="Context2.Provider[2]" />
+					</Context2.Provider>
+				</Context1.Provider>
+			</Context2.Provider>
+		</Context1.Provider>
+	);
+	expect(screen.getByTestId("Context2.defaultValue")).toHaveTextContent("default value");
+	expect(screen.getByTestId("Context1.Provider[0]")).toHaveTextContent("value1a");
+	expect(screen.getByTestId("Context2.Provider[0]")).toHaveTextContent("value2a");
+	expect(screen.getByTestId("Context1.Provider[1]")).toHaveTextContent("value1b");
+	expect(screen.getByTestId("Context2.Provider[1]")).toHaveTextContent("value2a");
+	expect(screen.getByTestId("Context1.Provider[2]")).toHaveTextContent("value1b");
+	expect(screen.getByTestId("Context2.Provider[2]")).toHaveTextContent("value2b");
+});
 
 test("ProxyContext", () =>
-	{
-		const RawContext = React.createContext("default value");
-		const Context1 = createProxyContext(RawContext, { contextId: "test4" });
+{
+	const RawContext = React.createContext("default value");
+	const Context1 = createProxyContext(RawContext, { contextId: "test4" });
 
-		render(<>
+	render(
+		<>
 			<Render context={Context1} testId="Context1.defaultValue" />
 			<RawContext.Provider value="value1">
 				<Render context={Context1} testId="Context1.Provider[0]" />
 			</RawContext.Provider>
-		</>);
+		</>
+	);
 
-		expect(screen.getByTestId("Context1.defaultValue")).toHaveTextContent("default value");
-		expect(screen.getByTestId("Context1.Provider[0]")).toHaveTextContent("value1");
-	});
+	expect(screen.getByTestId("Context1.defaultValue")).toHaveTextContent("default value");
+	expect(screen.getByTestId("Context1.Provider[0]")).toHaveTextContent("value1");
+});
 
 test("Context.Consumer", () =>
-	{
-		const Context1 = createContext("default value", { contextId: "test5" });
+{
+	const Context1 = createContext("default value", { contextId: "test5" });
 
-		render(<>
-			<Context1.Consumer>{
-				value => <span data-testid="Context1.defaultValue">{value}</span>
-			}</Context1.Consumer>
+	render(
+		<>
+			<Context1.Consumer>
+				{ (value) => <span data-testid="Context1.defaultValue">{value}</span> }
+			</Context1.Consumer>
 			<Context1.Provider value="value1">
-				<Context1.Consumer>{
-					value => <span data-testid="Context1.Provider[0]">{value}</span>
-				}</Context1.Consumer>
+				<Context1.Consumer>
+					{ (value) => <span data-testid="Context1.Provider[0]">{value}</span> }
+				</Context1.Consumer>
 			</Context1.Provider>
-		</>);
+		</>
+	);
 
-		expect(screen.getByTestId("Context1.defaultValue")).toHaveTextContent("default value");
-		expect(screen.getByTestId("Context1.Provider[0]")).toHaveTextContent("value1");
-	});
+	expect(screen.getByTestId("Context1.defaultValue")).toHaveTextContent("default value");
+	expect(screen.getByTestId("Context1.Provider[0]")).toHaveTextContent("value1");
+});
 
 test("useContexts", () =>
-	{
-		const Context1 = createContext("default value1", { contextId: "test6" });
-		const Context2 = createContext("default value2", { contextId: "test7" });
+{
+	const Context1 = createContext("default value1", { contextId: "test6" });
+	const Context2 = createContext("default value2", { contextId: "test7" });
 
-		const UseContextsArray = ({ testId }: { testId: string }) =>
-			<span data-testid={testId}>{
-				useContexts([Context1, Context2, Context1]).join(",")
-			}</span>
+	const UseContextsArray = ({ testId }: { testId: string }) => (
+		<span data-testid={testId}>
+			{ useContexts([Context1, Context2, Context1]).join(",") }
+		</span>
+	);
 
-		const UseContextsObject = () =>
-			<>{
-				Object.entries(useContexts({ one: Context1, two: Context2, three: Context2 }))
-					.map(([key, value], index) =>
-						<div key={index}>
-							<span data-testid={`Context:${key}`}>{value}</span>
-						</div>)
-			}</>
+	const UseContextsObject = () => (
+		<>
+			{ Object.entries(useContexts({ one: Context1, two: Context2, three: Context2 }))
+				.map(([key, value]) => (
+					<div key={key}>
+						<span data-testid={`Context:${key}`}>{value}</span>
+					</div>)) }
+		</>
+	);
 
-		render(<>
+	render(
+		<>
 			<UseContextsArray testId="use-contexts-array-0" />
 			<Context1.Provider value="value1">
 				<Context2.Provider value="value2">
 					<UseContextsArray testId="use-contexts-array-1" />
-					<UseContextsObject/>
+					<UseContextsObject />
 				</Context2.Provider>
 			</Context1.Provider>
-		</>);
+		</>
+	);
 
-		expect(screen.getByTestId("use-contexts-array-0")).toHaveTextContent("default value1,default value2,default value1");
-		expect(screen.getByTestId("use-contexts-array-1")).toHaveTextContent("value1,value2,value1");
-		expect(screen.getByTestId("Context:one")).toHaveTextContent("value1");
-		expect(screen.getByTestId("Context:two")).toHaveTextContent("value2");
-		expect(screen.getByTestId("Context:three")).toHaveTextContent("value2");
-	});
+	expect(screen.getByTestId("use-contexts-array-0"))
+		.toHaveTextContent("default value1,default value2,default value1");
+	expect(screen.getByTestId("use-contexts-array-1")).toHaveTextContent("value1,value2,value1");
+	expect(screen.getByTestId("Context:one")).toHaveTextContent("value1");
+	expect(screen.getByTestId("Context:two")).toHaveTextContent("value2");
+	expect(screen.getByTestId("Context:three")).toHaveTextContent("value2");
+});
 
 test("initialValue", () =>
+{
+	const Context1 = createContext("default value", { contextId: "test8" });
+
+	const Updater = ({ value }: { value: string }) =>
 	{
-		const Context1 = createContext("default value", { contextId: "test8" });
+		const update = useContextUpdate(Context1);
+		useEffect(() => update(value), [update, value]);
+		return null;
+	};
 
-		const Updater = ({ value }: { value: string }) =>
-			{
-				const update = useContextUpdate(Context1);
-				useEffect(() => update(value), [value]);
-				return null;
-			}
+	const ShowIfContextEquals = ({ value, children }: { value: string, children: ReactElement }) =>
+	{
+		const contextValue = useContext(Context1);
+		return value === contextValue ? children : null;
+	};
 
-		const ShowIfContextEquals = ({ value, children }: { value: string, children: ReactNode }) =>
-			{
-				const contextValue = useContext(Context1);
-				return (value === contextValue) ? <>{children}</> : null;
-			}
+	const ChangingInitialValue = () =>
+	{
+		const [value, setValue] = useState("value6");
 
-		const ChangingInitialValue = () =>
-			{
-				const [value, setValue] = useState("value6");
+		useEffect(() => setValue("value7"), []);
 
-				useEffect(() => setValue("value7"));
+		return (
+			<Context1.Provider initialValue={value}>
+				<span data-testid="changedValue">{value}</span>
+				<Render context={Context1} testId="Context1.Provider[5]" />
+			</Context1.Provider>
+		);
+	};
 
-				return (
-					<Context1.Provider initialValue={value}>
-						<span data-testid="changedValue">{value}</span>
-						<Render context={Context1} testId="Context1.Provider[5]" />
-					</Context1.Provider>
-				);
-			}
-
-		render(<>
+	render(
+		<>
 			<Context1.Provider initialValue="value0">
 				<Render context={Context1} testId="Context1.Provider[0]" />
 			</Context1.Provider>
@@ -186,29 +211,30 @@ test("initialValue", () =>
 				<Render context={Context1} testId="Context1.Provider[2]" />
 			</Context1.Provider>
 			<Context1.Provider value="value2">
-				<Updater value="value3"/>
+				<Updater value="value3" />
 				<Context1.Provider initialValue={INHERIT}>
 					<Render context={Context1} testId="Context1.Provider[3]" />
 				</Context1.Provider>
 			</Context1.Provider>
 			<Context1.Provider value="value4">
-				<Updater value="value5"/>
+				<Updater value="value5" />
 				<ShowIfContextEquals value="value5">
 					<Context1.Provider initialValue={INHERIT}>
 						<Render context={Context1} testId="Context1.Provider[4]" />
 					</Context1.Provider>
 				</ShowIfContextEquals>
 			</Context1.Provider>
-			<ChangingInitialValue/>
-		</>);
+			<ChangingInitialValue />
+		</>
+	);
 
-		expect(screen.getByTestId("Context1.Provider[0]")).toHaveTextContent("value0");
-		expect(screen.getByTestId("Context1.Provider[1]")).toHaveTextContent("value1");
-		expect(screen.getByTestId("Context1.Provider[2]")).toHaveTextContent("default value");
-		expect(screen.getByTestId("Context1.Provider[3]")).toHaveTextContent("value2");
-		expect(screen.getByTestId("Context1.Provider[4]")).toHaveTextContent("value5");
+	expect(screen.getByTestId("Context1.Provider[0]")).toHaveTextContent("value0");
+	expect(screen.getByTestId("Context1.Provider[1]")).toHaveTextContent("value1");
+	expect(screen.getByTestId("Context1.Provider[2]")).toHaveTextContent("default value");
+	expect(screen.getByTestId("Context1.Provider[3]")).toHaveTextContent("value2");
+	expect(screen.getByTestId("Context1.Provider[4]")).toHaveTextContent("value5");
 
-		// Changing initialValue prop doesn't change the provider's value
-		expect(screen.getByTestId("changedValue")).toHaveTextContent("value7");
-		expect(screen.getByTestId("Context1.Provider[5]")).toHaveTextContent("value6");
-	});
+	// Changing initialValue prop doesn't change the provider's value
+	expect(screen.getByTestId("changedValue")).toHaveTextContent("value7");
+	expect(screen.getByTestId("Context1.Provider[5]")).toHaveTextContent("value6");
+});
